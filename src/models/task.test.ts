3bc4d664accb14059291a3d0190840e3c7367806
ujiro99 <siro.cola@gmail.com>
2022-04-10 22:57:15 +0900

    add: Enables to parse the estimated time and tags.

--- src/models/task.test.ts
+++ src/models/task.test.ts
@@ -1,44 +1,85 @@
 import { Task } from '@/models/task'
 
 const testParseTable = [
-  ["- [ ] task title", "task title", "", false],
-  ["- [x] task title", "task title", "", true],
-  ["- [ ] tasktitle ~2h", "tasktitle", "2h", false],
-  ["- [ ] tasktitle ~1m", "tasktitle", "1m", false],
-  ["- [ ] tasktitle ~2.1h", "tasktitle", "2h6m", false],
-  ["- [ ] tasktitle ~2.1h4m", "tasktitle", "2h10m", false],
-  ["- [ ] tasktitle ~0.1d1h1m", "tasktitle", "3h25m", false],
-  ["- [ ] tasktitle ~1.1d1h1m", "tasktitle", "1d3h25m", false],
-  ["- [ ] task title ~30m #sp:1", "task title", "30m", false],
+  ['- [ ] task title', 'task title', '', false],
+  ['- [x] task title', 'task title', '', true],
+  ['- [ ] tasktitle ~2h', 'tasktitle', '2h', false],
+  ['- [ ] tasktitle ~1m', 'tasktitle', '1m', false],
+  ['- [ ] tasktitle ~2.1h', 'tasktitle', '2h6m', false],
+  ['- [ ] tasktitle ~2.1h4m', 'tasktitle', '2h10m', false],
+  ['- [ ] tasktitle ~0.1d1h1m', 'tasktitle', '3h25m', false],
+  ['- [ ] tasktitle ~1.1d1h1m', 'tasktitle', '1d3h25m', false],
+  ['- [ ] task title ~30m #sp:1', 'task title', '30m', false],
+  ['- [ ] task title ~30m/2.3h #sp:1', 'task title', '30m', false],
 ]
 
-describe.each(testParseTable)(`parse %s`, (str: string, title: string, time: string, state: boolean) => {
-  test(`returns ${title} ${time}`, () => {
+describe.each(testParseTable)(
+  `parse %s`,
+  (str: string, title: string, time: string, state: boolean) => {
+    test(`returns ${title} ${time}`, () => {
+      const task = Task.parse(str)
+      expect(task.title).toBe(title)
+      expect(task.actualTimes.toString()).toBe(time)
+      expect(task.isComplete()).toBe(state)
+    })
+  },
+)
+
+describe(`toString`, () => {
+  test(`returns same string`, () => {
+    const str = '- [ ] task title ~30m/2h15m #sp:1'
     const task = Task.parse(str)
-    expect(task.title).toBe(title)
-    expect(task.actualTimes.toString()).toBe(time)
-    expect(task.isComplete()).toBe(state)
+    expect(task.toString()).toBe(str)
+  })
+})
+
+describe(`parse estimatedTimes`, () => {
+  test(`returns 1h`, () => {
+    const str = '- [ ] task title ~30m/1h'
+    const task = Task.parse(str)
+    expect(task.estimatedTimes.toString()).toBe('1h')
+  })
+})
+
+describe(`parse tags`, () => {
+  test(`returns sp:1`, () => {
+    const str = '- [ ] task title ~30m/1h #sp:1'
+    const task = Task.parse(str)
+    expect(task.tags[0].name).toBe('sp:1')
+  })
+
+  test(`returns sp:1 and review`, () => {
+    const str = '- [ ] task title ~30m/1h #sp:1 #review'
+    const task = Task.parse(str)
+    expect(task.tags[0].name).toBe('sp:1')
+    expect(task.tags[1].name).toBe('review')
+  })
+
+  test(`returns no tags`, () => {
+    const str = '- [ ] task title'
+    const task = Task.parse(str)
+    expect(task.tags.length).toBe(0)
   })
 })
 
 describe('trackingStart', () => {
   test('changes its state', () => {
-    const task = Task.parse("- [ ] task title")
+    const task = Task.parse('- [ ] task title')
     expect(task.isRunning()).toBe(false)
     task.trackingStart()
     expect(task.isRunning()).toBe(true)
-  });
+  })
   test('returns current time', () => {
-    const task = Task.parse("- [ ] task title")
+    const task = Task.parse('- [ ] task title')
     expect(task.isRunning()).toBe(false)
     const now = task.trackingStart()
     expect(now > 0).toBe(true)
-  });
+  })
 })
 
 describe('trackingStop', () => {
   test('changes its state', () => {
-    const task = Task.parse("- [ ] task title")
+    const task = Task.parse('- [ ] task title')
     const now = task.trackingStart()
     expect(task.isRunning()).toBe(true)
     task.trackingStop(now)
@@ -46,14 +87,14 @@ describe('trackingStop', () => {
   })
 
   test('updates actual times: 1 minutes', () => {
-    const task = Task.parse("- [ ] task title")
+    const task = Task.parse('- [ ] task title')
     const now = task.trackingStart()
     task.trackingStop(now - 60 * 1000)
     expect(task.actualTimes.minutes).toBe(1)
   })
 
   test('updates actual times: 1hours, 1 minutes', () => {
-    const task = Task.parse("- [ ] task title")
+    const task = Task.parse('- [ ] task title')
     const now = task.trackingStart()
     task.trackingStop(now - (60 * 60 * 1000 + 60 * 1000))
     expect(task.actualTimes.minutes).toBe(1)
