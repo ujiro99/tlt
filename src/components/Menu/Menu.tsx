9fa87d8fb1f4001e0a8316c319932cafd470fd02
ujiro99 <siro.cola@gmail.com>
2022-05-29 20:50:25 +0900

    update: Change the look of the report menu.

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,15 +1,19 @@
 import React, { useState, useEffect } from 'react'
 
+import { useMode, MODE } from '@/hooks/useMode'
 import { Calendar } from '@/components/Menu/Calendar'
 import { ReportSummary } from '@/components/Menu/ReportSummary'
 import { Copy } from '@/components/Menu/Copy'
 import { Edit } from '@/components/Menu/Edit'
-import {  sleep } from '@/services/util'
+import { Icon } from '@/components/Icon'
+import { sleep } from '@/services/util'
 
 import './Menu.css'
 
 export function Menu(): JSX.Element {
   const [isFixed, setFixed] = useState(false)
+  const [mode, setMode] = useMode()
+  const isReport = mode === MODE.REPORT
 
   useEffect(() => {
     let intersectionObserver: IntersectionObserver
@@ -31,6 +35,10 @@ export function Menu(): JSX.Element {
     }
   }, [])
 
+  const backTodo = () => {
+    setMode(MODE.SHOW)
+  }
+
   return (
     <>
       <header className="menu__sticky">
@@ -39,9 +47,14 @@ export function Menu(): JSX.Element {
           <Copy />
         </div>
       </header>
+      {isReport && (
+        <button className="menu__back" onClick={backTodo}>
+          <Icon name="arrow-back" />
+        </button>
+      )}
       <div className="menu">
         <Calendar fixed={isFixed} />
-        <ReportSummary fixed={isFixed} />
+        {!isReport && <ReportSummary fixed={isFixed} />}
       </div>
     </>
   )
