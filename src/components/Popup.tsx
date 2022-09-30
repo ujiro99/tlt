878975d8caf4af78a18854a7bfbf695386625798
ujiro99 <siro.cola@gmail.com>
2022-09-30 12:29:42 +0900

    実装途中

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -1,5 +1,7 @@
-import React, { useEffect } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { RecoilRoot } from 'recoil'
+import { ShepherdTour, ShepherdTourContext } from 'react-shepherd'
+
 import { ErrorFallback } from '@/components/ErrorFallback'
 import { TaskTextarea } from '@/components/TaskTextarea'
 import { useMode, MODE } from '@/hooks/useMode'
@@ -12,10 +14,78 @@ import { useTaskStorage } from '@/hooks/useTaskManager'
 import '@/css/common.css'
 import '@/components/Popup.css'
 
+const tourOptions = {
+  defaultStepOptions: {
+    cancelIcon: {
+      enabled: true
+    }
+  },
+  useModalOverlay: true
+};
+
+function Button() {
+  const tour = useContext(ShepherdTourContext);
+
+  return (
+    <button className="button dark" onClick={tour.start}>
+      Start Tour
+    </button>
+  );
+}
+
 export default function Popup(): JSX.Element {
   useEffect(() => {
     chrome.runtime.sendMessage({ command: 'popupMounted' })
   }, [])
+
+  const steps = [
+    {
+      id: 'intro',
+      attachTo: { element: '.first-element' },
+      beforeShowPromise: function () {
+        return new Promise<void>(function (resolve) {
+          setTimeout(function () {
+            window.scrollTo(0, 0);
+            resolve();
+          }, 500);
+        });
+      },
+      buttons: [
+        {
+          classes: 'shepherd-button-secondary',
+          text: 'Exit',
+          type: 'cancel'
+        },
+        {
+          classes: 'shepherd-button-primary',
+          text: 'Back',
+          type: 'back'
+        },
+        {
+          classes: 'shepherd-button-primary',
+          text: 'Next',
+          type: 'next'
+        }
+      ],
+      classes: 'custom-class-name-1 custom-class-name-2',
+      highlightClass: 'highlight',
+      scrollTo: false,
+      cancelIcon: {
+        enabled: true,
+      },
+      title: 'Welcome to React-Shepherd!',
+      text: ['React-Shepherd is a JavaScript library for guiding users through your React app.'],
+      when: {
+        show: () => {
+          console.log('show step');
+        },
+        hide: () => {
+          console.log('hide step');
+        }
+      }
+    },
+    // ...
+  ];
 
   return (
     <ErrorFallback>
@@ -24,6 +94,9 @@ export default function Popup(): JSX.Element {
           <Init />
           <Menu />
           <TaskList />
+          <ShepherdTour steps={steps} tourOptions={tourOptions}>
+            <Button />
+          </ShepherdTour>
         </React.Suspense>
       </RecoilRoot>
     </ErrorFallback>
