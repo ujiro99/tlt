d5fce217ecab3f217f1be34e64fd140c3a3a994d
ujiro99 <siro.cola@gmail.com>
2023-02-06 09:53:19 +0900

    chrome アプリとして呼ぶ場合

--- src/index.tsx
+++ src/index.tsx
@@ -14,6 +14,29 @@ document.addEventListener(
     analytics.track('launch', 'index')
     const root = createRoot(document.getElementById('popup'))
     root.render(<Popup />)
+
+    chrome.identity.getAuthToken({ interactive: true }, function (token) {
+      console.log(token)
+
+      var init = {
+        method: 'GET',
+        async: true,
+        headers: {
+          Authorization: 'Bearer ' + token,
+          'Content-Type': 'application/json',
+        },
+        contentType: 'json',
+      }
+
+      fetch(
+        'https://www.googleapis.com/calendar/v3/calendars/primary/events',
+        init,
+      )
+        .then((response) => response.json()) // Transform the data into json
+        .then(function (data) {
+          console.log(data)
+        })
+    })
   },
   false,
 )
