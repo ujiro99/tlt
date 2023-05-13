c68d7a314bfbfa0cddb52207abd4dc9d34ed3bec
ujiro99 <siro.cola@gmail.com>
2023-05-13 13:03:36 +0900

    udpate: Corrected alarm description wording.

--- src/components/Alarm/AlarmModal.tsx
+++ src/components/Alarm/AlarmModal.tsx
@@ -7,12 +7,51 @@ import { useAnalytics } from '@/hooks/useAnalytics'
 import { AlarmTaskTextarea } from '@/components/Alarm/AlarmTextarea'
 import { Icon } from '@/components/Icon'
 import { t } from '@/services/i18n'
+import { Alarm, ALARM_TYPE } from '@/models/alarm'
 
 import '../ModalWindow.css'
 import './AlarmList.css'
 
 // Make sure to bind modal to your appElement (https://reactcommunity.org/react-modal/accessibility/)
 Modal.setAppElement(document.getElementById('popup'))
+
+function AlarmItem({ alarm }: { alarm: Alarm }): JSX.Element {
+  if (alarm.type === ALARM_TYPE.EVENT) {
+    return <AlarmItemEvent alarm={alarm} />
+  }
+  if (alarm.type === ALARM_TYPE.TASK) {
+    return <AlarmItemTask alarm={alarm} />
+  }
+}
+
+function AlarmItemEvent({ alarm }: { alarm: Alarm }): JSX.Element {
+  return (
+    <>
+      <p>
+        <span className="alarm-list__time">{alarm.time}</span>
+      </p>
+      <p className="alarm-list__content">
+        <span className="alarm-list__name">
+          {t('alarm_event_description', [alarm.name])}
+        </span>
+      </p>
+    </>
+  )
+}
+
+function AlarmItemTask({ alarm }: { alarm: Alarm }): JSX.Element {
+  return (
+    <>
+      <p>
+        <span className="alarm-list__time">{alarm.time}</span>
+      </p>
+      <p className="alarm-list__content">
+        <span className="alarm-list__name">{alarm.name}</span>
+        <span className="alarm-list__message">{alarm.message}</span>
+      </p>
+    </>
+  )
+}
 
 export function AlarmModal(): JSX.Element {
   const analytics = useAnalytics()
@@ -47,31 +86,24 @@ export function AlarmModal(): JSX.Element {
         <section className="modal-window__section">
           <h3 className="modal-window__section-title">
             <Icon name="alart" />
-            {t("alarm_current")}
+            {t('alarm_current')}
           </h3>
           <ul className="alarm-list">
             {alarmExists ? (
               alarms.map((alarm) => (
                 <li className="alarm-list__item" key={alarm.toKey()}>
-                  <p>
-                    <span className="alarm-list__time">{alarm.time}</span>
-                  </p>
-                  <p className="alarm-list__content">
-                    <span className="alarm-list__name">{alarm.name}</span>
-                    <span className="alarm-list__message">{alarm.message}</span>
-                  </p>
+                  <AlarmItem alarm={alarm} />
                 </li>
               ))
             ) : (
               <p className="alarm-list__empty">{t('no_alarms')}</p>
-            )
-            }
+            )}
           </ul>
         </section>
         <section className="modal-window__section">
           <h3 className="modal-window__section-title">
             <Icon name="alart" />
-            {t("alarm_for_task")}
+            {t('alarm_for_task')}
           </h3>
           <AlarmTaskTextarea />
         </section>
