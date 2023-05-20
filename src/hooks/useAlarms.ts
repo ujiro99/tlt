12c920a88205a582b1afe93722bf31c8ad583734
ujiro99 <siro.cola@gmail.com>
2023-05-20 12:35:45 +0900

    refactoring.

--- src/hooks/useAlarms.ts
+++ src/hooks/useAlarms.ts
@@ -2,31 +2,15 @@ import { atom, selector, useRecoilState } from 'recoil'
 import { useCallback } from 'react'
 import { useStorage } from '@/hooks/useStorage'
 import { STORAGE_KEY } from '@/services/storage'
-import { t } from '@/services/i18n'
+import { AlarmService } from '@/services/alarm'
 import { Ipc } from '@/services/ipc'
 import { Task } from '@/models/task'
-import { Alarm, ALARM_TYPE } from '@/models/alarm'
-import { AlarmRule, ALARM_ANCHOR, ALARM_TIMING } from '@/models/alarmRule'
+import { Alarm } from '@/models/alarm'
+import { AlarmRule } from '@/models/alarmRule'
 import Log from '@/services/log'
 
 // for debug
 // chrome.alarms.clearAll()
-
-/**
- * Get all alarms from chrome API.
- * @returns Promise<Alarm[]>
- */
-const getAlarms = async (): Promise<Alarm[]> => {
-  const all = await chrome.alarms.getAll()
-  const alarms = all
-    .map((a) => {
-      return Alarm.fromString(a.name)
-    })
-    .filter((a) => a.type !== ALARM_TYPE.ICON)
-    .sort((a, b) => a.scheduledTime - b.scheduledTime)
-  Log.d(alarms)
-  return alarms
-}
 
 type useAlarmsReturn = {
   alarms: Alarm[]
@@ -42,7 +26,7 @@ export const alarmState = atom({
     key: 'alarmStateSelector',
     get: async () => {
       Log.d(`get alarmStateSelector`)
-      return await getAlarms()
+      return await AlarmService.getAlarms()
     },
   }),
 })
@@ -64,7 +48,7 @@ export function useAlarms(): useAlarmsReturn {
         })
       })
       Promise.all(promises).then(async () => {
-        _setAlarms(await getAlarms())
+        _setAlarms(await AlarmService.getAlarms())
       })
     },
     [_setAlarms],
@@ -72,47 +56,15 @@ export function useAlarms(): useAlarmsReturn {
 
   const setAlarmsForTask = useCallback(
     async (task: Task) => {
-      const promises = alarmRules.map(async (alarm) => {
-        let minutes = 0
-        let message: string
-        if (alarm.anchor === ALARM_ANCHOR.START) {
-          if (alarm.timing === ALARM_TIMING.AFTER) {
-            minutes = alarm.minutes
-            message = t('alarm_after_start', [`${minutes}`])
-          }
-        } else if (alarm.anchor === ALARM_ANCHOR.SCEHEDULED) {
-          if (task.estimatedTimes.toMinutes() === 0) {
-            Log.d('alarm not set because scheduled time is 0.')
-            return
-          }
-          if (alarm.timing === ALARM_TIMING.BEFORE) {
-            minutes =
-              task.estimatedTimes.toMinutes() -
-              task.actualTimes.toMinutes() -
-              alarm.minutes
-            message = t('alarm_before_schedule', [`${alarm.minutes}`])
-          } else {
-            minutes =
-              task.estimatedTimes.toMinutes() -
-              task.actualTimes.toMinutes() +
-              alarm.minutes
-            message = t('alarm_after_schedule_message', [`${minutes}`])
-          }
-        }
-        if (minutes <= 0) return
-
+      const alarms = AlarmService.taskToAlarms(task, alarmRules)
+      const promises = alarms.map(async (alarm) => {
         return Ipc.send({
           command: 'setAlarm',
-          param: new Alarm({
-            type: ALARM_TYPE.TASK,
-            name: task.title,
-            message: message,
-            minutes: minutes,
-          }),
+          param: alarm,
         })
       })
       Promise.all(promises).then(async () => {
-        _setAlarms(await getAlarms())
+        _setAlarms(await AlarmService.getAlarms())
       })
     },
     [alarms, alarmRules],
@@ -120,12 +72,12 @@ export function useAlarms(): useAlarmsReturn {
 
   const stopAlarms = useCallback(async (alarms: Alarm[]) => {
     await Ipc.send({ command: 'stopAlarms', param: alarms })
-    _setAlarms(await getAlarms())
+    _setAlarms(await AlarmService.getAlarms())
   }, [])
 
   const stopAlarmsForTask = useCallback(async () => {
     await Ipc.send({ command: 'stopAlarmsForTask' })
-    _setAlarms(await getAlarms())
+    _setAlarms(await AlarmService.getAlarms())
   }, [])
 
   return {
