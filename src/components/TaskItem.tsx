148fa0c990c1a7ecfc58b57d7be523ae0502e802
ujiro99 <siro.cola@gmail.com>
2023-08-11 20:41:04 +0900

    fix: Fixed an issue where the playback time in the list was misaligned during measurement.

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useLayoutEffect, CSSProperties } from 'react'
+import React, { useState, useLayoutEffect, CSSProperties } from 'react'
 import classnames from 'classnames'
 
 import Log from '@/services/log'
